db58ad55c50947325cd1b9643ffa442a327b7445
Thomas Sullivan <tsullivan@128technology.com>
2018-02-23 15:50:35 -0500

    minion/python/get_serial.py - when retrieving serial/MAC, convert to lower case for use with Minion IDs and Hostnames

--- minion/python/get_serial.py
+++ minion/python/get_serial.py
@@ -6,7 +6,9 @@ gBadExprs=[]
 gBadExprs.append('0')
 gBadExprs.append('to be filled by O.E.M.')
 
-serno=lshw.get_serial_number()
+# Retrieve serial number.  Converto to Lower Case
+serno=lshw.get_serial_number().lower()
+
 if serno != '':
     for expr in gBadExprs:
         if re.match(expr, serno, re.IGNORECASE):
@@ -14,9 +16,10 @@ if serno != '':
             break
 
 # If no serial number is extracted, defaults to a MAC
+# Converto to Lower Case
 if serno == '':   
     serno=lshw.get_first_intf_mac()
     serno=re.sub(':', '', serno)
     serno='MAC' + serno
-
+    serno=serno.lower()
 print "%s" % serno
