e4b99653dabc26efc21600cf3b5b0ffb16724988
Lane Shields <lshields@128technology.com>
2018-03-26 17:56:56 -0400

    Added webapp-minion configuration

---
+++ webapp-minion/python/get_serial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import re
+import lshw
+
+gBadExprs=[]
+gBadExprs.append('0')
+gBadExprs.append('to be filled by O.E.M.')
+gBadExprs.append('default string')
+
+# Retrieve serial number.  Convert to Lower Case
+serno=lshw.get_serial_number().lower()
+
+if serno != '':
+    for expr in gBadExprs:
+        if re.match(expr, serno, re.IGNORECASE):
+            serno=''
+            break
+
+# If no serial number is extracted, defaults to a MAC
+# Convert to Lower Case
+if serno == '':   
+    serno=lshw.get_first_intf_mac()
+    serno=re.sub(':', '', serno)
+    serno='MAC' + serno
+    serno=serno.lower()
+print "%s" % serno
